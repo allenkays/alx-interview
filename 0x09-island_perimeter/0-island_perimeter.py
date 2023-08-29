748be8e449a9e31b7748f1ff6677159f96b04e06
allenkays <allenkays@tutanota.com>
2023-08-29 10:03:55 +0300

    Fix pycodestyle error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -25,10 +25,11 @@ def island_perimeter(grid):
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
 
-                if row > 0 and grid[row -1][col] == 1:
+                if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
 
     return perimeter
+
 
 if __name__ == "__main__":
     island_perimeter(grid)
